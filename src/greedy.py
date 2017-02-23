2634538f98311e2d4331e0f2d51aaa2a8cbcd301
Bartosz Marcinkowski <bm371613@gmail.com>
2017-02-23 21:00:00 +0100

    bugfix

--- src/greedy.py
+++ src/greedy.py
@@ -2,21 +2,28 @@ import collections
 
 
 def greedy(input, request_priority, cache_chooser):
-    output = collections.defaultdict(list)
+    output = collections.defaultdict(set)
     space_left = collections.defaultdict(lambda: input.cache_size)
 
     for request in sorted(input.requests, key=request_priority, reverse=True):
         endpoint = input.endpoints[request.endpoint_id]
         video_size = input.video_sizes[request.video_id]
+        if any(
+            request.video_id in output[cache_id]
+            for cache_id in endpoint.cache_latencies
+        ):
+            continue
         available_cache_space_left = {
             cache_id: space_left[cache_id]
-            for cache_id, latency
-            in endpoint.cache_latencies.items()
+            for cache_id
+            in endpoint.cache_latencies
             if space_left[cache_id] >= video_size
         }
-        chosen_cache_id = cache_chooser(video_size, available_cache_space_left)
-        output[chosen_cache_id].append(request.video_id)
-    return output
+        chosen_cache_id = cache_chooser(
+            available_cache_space_left=available_cache_space_left,
+        )
+        output[chosen_cache_id].add(request.video_id)
+        space_left[chosen_cache_id] -= video_size
 
 
 # request priorities
@@ -27,7 +34,7 @@ def priority_count(request):
 
 # cache choosers
 
-def best_fit(video_size, available_cache_space_left):
+def best_fit(available_cache_space_left, **_):
     return min(
         available_cache_space_left.items(),
         key=lambda pair: pair[1]
